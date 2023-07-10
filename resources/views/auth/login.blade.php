053b0f1ae9dceff12e3eaa266c978a26087d4d5d
eliab <eliabsiji@gmail.com>
2023-07-10 09:23:54 +0100

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -81,21 +81,19 @@
 <div class="d-flex flex-column flex-column-fluid flex-lg-row">
     <!--begin::Aside-->
     <div class="d-flex flex-center w-lg-50 pt-15 pt-lg-0 px-10">              
-        <!--begin::Aside-->
         <div class="d-flex flex-center flex-lg-start flex-column">              
             <!--begin::Logo-->
             <a href="{{ asset('html/assets/index.html')}}" class="mb-7">
-                <img alt="Logo" src="{{ asset('html/assets/assets/media/logos/custom-3.svg')}}"/>
+                <img alt="Logo" src="{{ asset('html/assets/assets/media/logos/scroll.png')}}" height="60px" width="60px"/>
             </a>    
             <!--end::Logo-->            
 
             <!--begin::Title-->
             <h2 class="text-white fw-normal m-0"> 
-                Branding tools designed for your business
+                Staff tools designed organisation daily operations
             </h2>  
             <!--end::Title-->         
         </div>
-        <!--begin::Aside-->    
     </div>
     <!--begin::Aside-->
 
