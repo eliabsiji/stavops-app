88e1a6c8110b36b9053a4b6e34762791f45fc777
Eliab <eliabsiji@gmail.com>
2023-07-14 16:43:35 -0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
 <!-- Mirrored from preview.keenthemes.com/metronic8/demo1/authentication/layouts/creative/sign-in.html')}} by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 12 Jun 2023 04:07:02 GMT -->
 <!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
 <head>
-        <title>Metronic - The World's #1 Selling Bootstrap Admin Template by Keenthemes</title>
+        <title>Login | StavOps</title>
         <meta charset="utf-8"/>
        
         <meta name="viewport" content="width=device-width, initial-scale=1"/>      
